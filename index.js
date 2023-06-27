f18e86098e121c13501305f43c4e93e69a5148c4
Afsheen Kiran <122640020+Afsheen96@users.noreply.github.com>
2023-06-27 13:56:16 +0100

    add click event function in index.js

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
   $(".dropdown-btn").click(function () {
     $(".dropdown-content").toggle("close");
   });
+  $("#navbar").click(function () {
+    $(".navbar-content").toggle("show");
+    $("#navbar-icon").hide();
+
+    $("#icon-close").show();
+  });
 });
