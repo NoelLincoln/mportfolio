107fe04e4973879724b33cf9d3c935826b0b95aa
Noel Bryant <noelsobryant@gmail.com>
2023-06-28 12:34:22 +0300

    Fix hide mobile menu when link is clicked:     -use javascript queryselector to close menu when link     is clicked

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ const icon_close_ = document.querySelector(".icon-close");
 
 const icon_close = document.querySelector(".icon-close");
 
-const navbar_options = document.querySelector("navbar-options");
+const navbar_options_portfolio = document.querySelector(
+  ".navbar-options-portfolio"
+);
+const navbar_options_about = document.querySelector(".navbar-options-about");
+const navbar_options_contact = document.querySelector(
+  ".navbar-options-contact"
+);
+
 const headline_wrapper = document.querySelector(".headline");
 const logo = document.querySelector(".logo");
 const header = document.querySelector("header");
 
 function blurbackground() {
   headline_wrapper.style.filter = "blur(6px)";
+  logo.style.filter = "blur(6px)";
 }
 function undoblurbackground() {
   headline_wrapper.style.filter = "none";
+  logo.style.filter = "none";
 }
 
 mobile_nav.addEventListener("click", () => {
@@ -30,8 +39,22 @@ icon_close.addEventListener("click", () => {
   icon_close.classList.toggle("hide");
   undoblurbackground();
 });
-navbar_options.addEventListener("click", () => {
+
+navbar_options_portfolio.addEventListener("click", () => {
   navbar_content.classList.toggle("hide");
   mobile_nav_img.classList.toggle("hide");
   icon_close.classList.toggle("hide");
+  undoblurbackground();
+});
+navbar_options_about.addEventListener("click", () => {
+  navbar_content.classList.toggle("hide");
+  mobile_nav_img.classList.toggle("hide");
+  icon_close.classList.toggle("hide");
+  undoblurbackground();
+});
+navbar_options_contact.addEventListener("click", () => {
+  navbar_content.classList.toggle("hide");
+  mobile_nav_img.classList.toggle("hide");
+  icon_close.classList.toggle("hide");
+  undoblurbackground();
 });
