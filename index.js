11fe84b214d73d19b2d7d6b761e3d99dccc3dd2d
Noel Bryant <noelsobryant@gmail.com>
2023-06-28 13:31:34 +0300

    conigure eslint and fix linter errors in index.js

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
-const mobile_nav = document.querySelector(".mobile-nav");
-const navbar_content = document.querySelector(".navbar-content");
-const mobile_nav_img = document.querySelector(".mobile-nav-img");
-const icon_close_ = document.querySelector(".icon-close");
+const MobileNav = document.querySelector('.mobile-nav');
+const NavbarContent = document.querySelector('.navbar-content');
+const MobileNavImg = document.querySelector('.mobile-nav-img');
 
-const icon_close = document.querySelector(".icon-close");
+const IconClose = document.querySelector('.icon-close');
 
-const navbar_options_portfolio = document.querySelector(
-  ".navbar-options-portfolio"
+const NavbarOptionsPortfolio = document.querySelector(
+  '.navbar-options-portfolio',
 );
-const navbar_options_about = document.querySelector(".navbar-options-about");
-const navbar_options_contact = document.querySelector(
-  ".navbar-options-contact"
-);
+const NavbarOptionsAbout = document.querySelector('.navbar-options-about');
+const NavbarOptionsContact = document.querySelector('.navbar-options-contact');
 
-const headline_wrapper = document.querySelector(".headline");
-const logo = document.querySelector(".logo");
-const header = document.querySelector("header");
+const HeadlineWrapper = document.querySelector('.headline');
+const logo = document.querySelector('.logo');
+// const header = document.querySelector("header");
 
 function blurbackground() {
-  headline_wrapper.style.filter = "blur(6px)";
-  logo.style.filter = "blur(6px)";
+  HeadlineWrapper.style.filter = 'blur(6px)';
+  logo.style.filter = 'blur(6px)';
 }
 function undoblurbackground() {
-  headline_wrapper.style.filter = "none";
-  logo.style.filter = "none";
+  HeadlineWrapper.style.filter = 'none';
+  logo.style.filter = 'none';
 }
 
-mobile_nav.addEventListener("click", () => {
-  navbar_content.classList.toggle("hide");
-  mobile_nav_img.classList.toggle("hide");
-  icon_close.classList.toggle("hide");
+MobileNav.addEventListener('click', () => {
+  NavbarContent.classList.toggle('hide');
+  MobileNavImg.classList.toggle('hide');
+  IconClose.classList.toggle('hide');
   blurbackground();
 });
 
-icon_close.addEventListener("click", () => {
-  navbar_content.classList.toggle("hide");
-  mobile_nav_img.classList.toggle("hide");
-  icon_close.classList.toggle("hide");
+IconClose.addEventListener('click', () => {
+  NavbarContent.classList.toggle('hide');
+  MobileNavImg.classList.toggle('hide');
+  IconClose.classList.toggle('hide');
   undoblurbackground();
 });
 
-navbar_options_portfolio.addEventListener("click", () => {
-  navbar_content.classList.toggle("hide");
-  mobile_nav_img.classList.toggle("hide");
-  icon_close.classList.toggle("hide");
+NavbarOptionsPortfolio.addEventListener('click', () => {
+  NavbarContent.classList.toggle('hide');
+  MobileNavImg.classList.toggle('hide');
+  IconClose.classList.toggle('hide');
   undoblurbackground();
 });
-navbar_options_about.addEventListener("click", () => {
-  navbar_content.classList.toggle("hide");
-  mobile_nav_img.classList.toggle("hide");
-  icon_close.classList.toggle("hide");
+NavbarOptionsAbout.addEventListener('click', () => {
+  NavbarContent.classList.toggle('hide');
+  MobileNavImg.classList.toggle('hide');
+  IconClose.classList.toggle('hide');
   undoblurbackground();
 });
-navbar_options_contact.addEventListener("click", () => {
-  navbar_content.classList.toggle("hide");
-  mobile_nav_img.classList.toggle("hide");
-  icon_close.classList.toggle("hide");
+NavbarOptionsContact.addEventListener('click', () => {
+  NavbarContent.classList.toggle('hide');
+  MobileNavImg.classList.toggle('hide');
+  IconClose.classList.toggle('hide');
   undoblurbackground();
 });
