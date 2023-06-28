a58f02eebfbf383d986c7a6c0112e0e5cab8c3d1
Noel Bryant <noelsobryant@gmail.com>
2023-06-28 11:29:56 +0300

    Style mobile menu and use Javascript class selectors:     -toggle blur on the background when menu icon is closed

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-$(document).ready(() => {
-  $('.dropdown-btn').click(() => {
-    $('.dropdown-content').toggle('close');
-  });
-  $('#navbar').click(() => {
-    $('.navbar-content').toggle('show');
-    $('#navbar-icon').hide();
+const mobile_nav = document.querySelector(".mobile-nav");
+const navbar_content = document.querySelector(".navbar-content");
+const mobile_nav_img = document.querySelector(".mobile-nav-img");
+const icon_close_ = document.querySelector(".icon-close");
 
-    $('#icon-close').show();
-  });
+const icon_close = document.querySelector(".icon-close");
+
+const navbar_options = document.querySelector("navbar-options");
+const headline_wrapper = document.querySelector(".headline");
+const logo = document.querySelector(".logo");
+const header = document.querySelector("header");
+
+function blurbackground() {
+  headline_wrapper.style.filter = "blur(6px)";
+}
+function undoblurbackground() {
+  headline_wrapper.style.filter = "none";
+}
+
+mobile_nav.addEventListener("click", () => {
+  navbar_content.classList.toggle("hide");
+  mobile_nav_img.classList.toggle("hide");
+  icon_close.classList.toggle("hide");
+  blurbackground();
+});
+
+icon_close.addEventListener("click", () => {
+  navbar_content.classList.toggle("hide");
+  mobile_nav_img.classList.toggle("hide");
+  icon_close.classList.toggle("hide");
+  undoblurbackground();
+});
+navbar_options.addEventListener("click", () => {
+  navbar_content.classList.toggle("hide");
+  mobile_nav_img.classList.toggle("hide");
+  icon_close.classList.toggle("hide");
 });
