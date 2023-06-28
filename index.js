f285ba45b0d00b1bfd5476029234492609828ba3
Noel Bryant <noelsobryant@gmail.com>
2023-06-28 19:27:47 +0300

    remove duplicate lines of code for navbaroptions onclick event

--- index.js
+++ index.js
@@ -1,57 +1,42 @@
-const MobileNav = document.querySelector('.mobile-nav');
-const NavbarContent = document.querySelector('.navbar-content');
-const MobileNavImg = document.querySelector('.mobile-nav-img');
+const MobileNav = document.querySelector(".mobile-nav");
+const NavbarContent = document.querySelector(".navbar-content");
+const MobileNavImg = document.querySelector(".mobile-nav-img");
 
-const IconClose = document.querySelector('.icon-close');
+const IconClose = document.querySelector(".icon-close");
 
-const NavbarOptionsPortfolio = document.querySelector(
-  '.navbar-options-portfolio',
-);
-const NavbarOptionsAbout = document.querySelector('.navbar-options-about');
-const NavbarOptionsContact = document.querySelector('.navbar-options-contact');
+const NavbarOptions = document.querySelectorAll(".navbar-options");
 
-const HeadlineWrapper = document.querySelector('.headline');
-const logo = document.querySelector('.logo');
-// const header = document.querySelector("header");
+const HeadlineWrapper = document.querySelector(".headline");
+const logo = document.querySelector(".logo");
 
 function blurbackground() {
-  HeadlineWrapper.style.filter = 'blur(6px)';
-  logo.style.filter = 'blur(6px)';
+  HeadlineWrapper.style.filter = "blur(6px)";
+  logo.style.filter = "blur(6px)";
 }
 function undoblurbackground() {
-  HeadlineWrapper.style.filter = 'none';
-  logo.style.filter = 'none';
+  HeadlineWrapper.style.filter = "none";
+  logo.style.filter = "none";
 }
 
-MobileNav.addEventListener('click', () => {
-  NavbarContent.classList.toggle('hide');
-  MobileNavImg.classList.toggle('hide');
-  IconClose.classList.toggle('hide');
+MobileNav.addEventListener("click", () => {
+  NavbarContent.classList.toggle("hide");
+  MobileNavImg.classList.toggle("hide");
+  IconClose.classList.toggle("hide");
   blurbackground();
 });
 
-IconClose.addEventListener('click', () => {
-  NavbarContent.classList.toggle('hide');
-  MobileNavImg.classList.toggle('hide');
-  IconClose.classList.toggle('hide');
+IconClose.addEventListener("click", () => {
+  NavbarContent.classList.toggle("hide");
+  MobileNavImg.classList.toggle("hide");
+  IconClose.classList.toggle("hide");
   undoblurbackground();
 });
 
-NavbarOptionsPortfolio.addEventListener('click', () => {
-  NavbarContent.classList.toggle('hide');
-  MobileNavImg.classList.toggle('hide');
-  IconClose.classList.toggle('hide');
-  undoblurbackground();
-});
-NavbarOptionsAbout.addEventListener('click', () => {
-  NavbarContent.classList.toggle('hide');
-  MobileNavImg.classList.toggle('hide');
-  IconClose.classList.toggle('hide');
-  undoblurbackground();
-});
-NavbarOptionsContact.addEventListener('click', () => {
-  NavbarContent.classList.toggle('hide');
-  MobileNavImg.classList.toggle('hide');
-  IconClose.classList.toggle('hide');
-  undoblurbackground();
+NavbarOptions.forEach(function (navbaroption) {
+  navbaroption.addEventListener("click", () => {
+    NavbarContent.classList.toggle("hide");
+    MobileNavImg.classList.toggle("hide");
+    IconClose.classList.toggle("hide");
+    undoblurbackground();
+  });
 });
