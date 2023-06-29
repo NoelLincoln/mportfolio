13fcf6a3f2beffe193e161633da432a9c92afdd0
Noel Bryant <noelsobryant@gmail.com>
2023-06-30 00:45:44 +0300

    use JSON object to store project details

--- index.js
+++ index.js
@@ -11,9 +11,144 @@ const projects = document.querySelector('.work-collection');
 const about = document.querySelector('.about-myself');
 const contact = document.querySelector('.contact-form');
 
-const SeeProject = document.querySelector('#see-project');
-const ProjectModal = document.querySelector('.project-modal');
-const ProjectClose = document.querySelector('.project-close');
+// const SnapshotImage = document.querySelector('#snapshot');
+// const ProjectName = document.querySelector('#projectname');
+
+fetch('projects.json')
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((project) => {
+      console.log(data);
+      console.log(project.name);
+
+      const WorksCollection = document.getElementById('workcollection');
+      const Works = document.createElement('div');
+      Works.classList.add('works');
+      WorksCollection.appendChild(Works);
+
+      const Snapshot = document.createElement('div');
+      Snapshot.classList.add('snapshot');
+      Snapshot.setAttribute('id', 'snapshot');
+      const SnapshotImage = document.createElement('img');
+      SnapshotImage.src = 'assets/images/snapshot.svg';
+      Snapshot.appendChild(SnapshotImage);
+
+      const CardInfo = document.createElement('div');
+      CardInfo.classList.add('card-info');
+      CardInfo.setAttribute('id', 'cardinfo');
+
+      Works.appendChild(Snapshot);
+      Works.appendChild(CardInfo);
+
+      const PrimaryText = document.createElement('div');
+      PrimaryText.classList.add('primary-text');
+      const Title = document.createElement('h2');
+      const TitleText = document.createTextNode('Tonic');
+      Title.appendChild(TitleText);
+
+      PrimaryText.appendChild(Title);
+      const WorkInfo = document.createElement('div');
+      WorkInfo.classList.add('work-info');
+      const Client = document.createElement('div');
+      Client.classList.add('client');
+      const ClientText = document.createElement('p');
+      const ClientTextNode = document.createTextNode('Canopy');
+      ClientText.appendChild(ClientTextNode);
+      Client.appendChild(ClientText);
+
+      const Counter = document.createElement('div');
+      Counter.classList.add('counter');
+
+      const Role = document.createElement('div');
+      Role.classList.add('role');
+      const RoleText = document.createElement('p');
+      Role.appendChild(RoleText);
+      const RoleTextNode = document.createTextNode('Backend Dev');
+      RoleText.appendChild(RoleTextNode);
+
+      const CounterOne = document.createElement('div');
+      CounterOne.classList.add('counterone');
+      const CounterOneText = document.createElement('p');
+      CounterOne.appendChild(CounterOneText);
+
+      const Year = document.createElement('div');
+      Year.classList.add('year');
+      const YearText = document.createElement('p');
+      Year.appendChild(YearText);
+      const YearTextNode = document.createTextNode('2023');
+      YearText.appendChild(YearTextNode);
+
+      RoleText.appendChild(RoleTextNode);
+      WorkInfo.appendChild(Client);
+      WorkInfo.appendChild(Counter);
+      WorkInfo.appendChild(Role);
+      WorkInfo.appendChild(CounterOne);
+
+      WorkInfo.appendChild(Year);
+
+      PrimaryText.appendChild(WorkInfo);
+
+      CardInfo.appendChild(PrimaryText);
+
+      const DescriptionText = document.createElement('div');
+      DescriptionText.classList.add('description-text');
+      const DescriptionTextNode = document.createTextNode(
+        'A daily selection of privately personalized reads; no    accounts or sign-ups required.'
+      );
+      DescriptionText.appendChild(DescriptionTextNode);
+      CardInfo.appendChild(DescriptionText);
+
+      const LanguageTags = document.createElement('ul');
+      LanguageTags.classList.add('language-tags');
+      const ListLanguages = document.createElement('li');
+      ListLanguages.classList.add('html-lan');
+      const ListLanguagesTextNode = document.createTextNode('html');
+      ListLanguages.appendChild(ListLanguagesTextNode);
+
+      const ListLanguagesCss = document.createElement('li');
+      const ListLanguagesCssTextNode = document.createTextNode('css');
+      ListLanguagesCss.classList.add('css');
+      ListLanguagesCss.appendChild(ListLanguagesCssTextNode);
+
+      const ListLanguagesJs = document.createElement('li');
+      const ListLanguagesJsTextNode = document.createTextNode('javscript');
+      ListLanguagesJs.classList.add('javascript');
+      ListLanguagesJs.appendChild(ListLanguagesJsTextNode);
+
+      const list = document.querySelectorAll('li');
+      list.forEach((language, index) => {
+        if (index === 0) {
+          language.classList.add('html');
+        } else if (index === 2) {
+          language.classList.add('css');
+        }
+      });
+      LanguageTags.appendChild(ListLanguages);
+      LanguageTags.appendChild(ListLanguagesCss);
+
+      LanguageTags.appendChild(ListLanguagesJs);
+
+      CardInfo.appendChild(LanguageTags);
+
+      const ActionButton = document.createElement('button');
+      ActionButton.classList.add('action-button');
+      ActionButton.setAttribute('id', 'see-project');
+      const ActionButtonText = document.createTextNode('See Project');
+      ActionButton.appendChild(ActionButtonText);
+
+      CardInfo.appendChild(ActionButton);
+
+      const SeeProject = document.querySelector('#see-project');
+      const ProjectModal = document.querySelector('.project-modal');
+      const ProjectClose = document.querySelector('.project-close');
+      SeeProject.addEventListener('click', () => {
+        ProjectModal.style.display = 'block';
+      });
+      ProjectClose.addEventListener('click', () => {
+        ProjectModal.style.display = 'none';
+      });
+    });
+  });
 
 function blurbackground() {
   HeadlineWrapper.style.filter = 'blur(6px)';
@@ -31,6 +166,14 @@ function undoblurbackground() {
 }
 
 MobileNav.addEventListener('click', () => {
+  const list = document.querySelectorAll('li');
+  list.forEach((language, index) => {
+    if (index === 0) {
+      language.classList.remove('html');
+    } else if (index === 2) {
+      language.classList.remove('css');
+    }
+  });
   NavbarContent.classList.toggle('hide');
   MobileNavImg.classList.toggle('hide');
   IconClose.classList.toggle('hide');
@@ -51,10 +194,4 @@ NavbarOptions.forEach((navbaroption) => {
     IconClose.classList.toggle('hide');
     undoblurbackground();
   });
-});
-SeeProject.addEventListener('click', () => {
-  ProjectModal.style.display = 'block';
-});
-ProjectClose.addEventListener('click', () => {
-  ProjectModal.style.display = 'none';
 });
