e899018645e602305ef5ff0f3b89a32015df6be4
Noel Bryant <noelsobryant@gmail.com>
2023-06-29 03:55:14 +0300

    fix blur on mobile nav

--- index.js
+++ index.js
@@ -5,19 +5,29 @@ const MobileNavImg = document.querySelector('.mobile-nav-img');
 const IconClose = document.querySelector('.icon-close');
 
 const NavbarOptions = document.querySelectorAll('.navbar-options');
-
 const HeadlineWrapper = document.querySelector('.headline');
 const logo = document.querySelector('.logo');
+const projects = document.querySelector('.work-collection');
+const about = document.querySelector('.about-myself');
+const contact = document.querySelector('.contact-form');
+
 const SeeProject = document.querySelector('#see-project');
 const ProjectModal = document.querySelector('.project-modal');
+const ProjectClose = document.querySelector('.project-close');
 
 function blurbackground() {
   HeadlineWrapper.style.filter = 'blur(6px)';
   logo.style.filter = 'blur(6px)';
+  projects.style.filter = 'blur(6px)';
+  about.style.filter = 'blur(6px)';
+  contact.style.filter = 'blur(6px)';
 }
 function undoblurbackground() {
   HeadlineWrapper.style.filter = 'none';
   logo.style.filter = 'none';
+  projects.style.filter = 'none';
+  about.style.filter = 'none';
+  contact.style.filter = 'none';
 }
 
 MobileNav.addEventListener('click', () => {
@@ -42,12 +52,9 @@ NavbarOptions.forEach((navbaroption) => {
     undoblurbackground();
   });
 });
-// seeProject.forEach((seeproject) => {
-//   seeproject.addEventListener('click', () => {
-//     ProjectModal.style.display = 'block';
-//   });
-// });
-
 SeeProject.addEventListener('click', () => {
   ProjectModal.style.display = 'block';
+});
+ProjectClose.addEventListener('click', () => {
+  ProjectModal.style.display = 'none';
 });
