aee0805a7f23641b37dbbdb2d5661cbd0c57961e
Noel Bryant <noelsobryant@gmail.com>
2023-07-01 14:33:35 +0300

    add local storage

--- formvalidation.js
+++ formvalidation.js
@@ -14,3 +14,45 @@ const submitform = document.getElementById('submitform');
 submitform.addEventListener('click', () => {
   validateEmail();
 });
+
+// Function to save form data to local storage
+function saveFormData() {
+  const form = document.getElementById('contactform');
+  const formData = {};
+
+  // Loop through each input field in the form
+  for (let i = 0; i < form.elements.length; i++) {
+    const element = form.elements[i];
+    if (element.type !== 'submit') {
+      formData[element.name] = element.value;
+    }
+  }
+
+  // Save the form data as a single object in local storage
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+// Function to load form data from local storage
+function loadFormData() {
+  const form = document.getElementById('contactform');
+  const storedData = localStorage.getItem('formData');
+
+  if (storedData) {
+    const formData = JSON.parse(storedData);
+
+    // Loop through each input field in the form
+    for (let i = 0; i < form.elements.length; i++) {
+      const element = form.elements[i];
+      if (element.type !== 'submit' && formData.hasOwnProperty(element.name)) {
+        element.value = formData[element.name];
+      }
+    }
+  }
+}
+
+// Load form data when the page is loaded
+window.addEventListener('load', loadFormData);
+
+// Save form data when any input field is changed
+const form = document.getElementById('myForm');
+form.addEventListener('input', saveFormData);
