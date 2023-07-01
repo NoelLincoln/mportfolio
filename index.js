f3d9c03bf8e0b4481765eb9c503c9b70193a4bbc
Afsheen Kiran <122640020+Afsheen96@users.noreply.github.com>
2023-07-01 09:33:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -216,20 +216,3 @@ NavbarOptions.forEach((navbaroption) => {
     undoblurbackground();
   });
 });
-
-function validateEmail() {
-  const emailInput = document.getElementById('email').value;
-  const lowerCaseEmail = emailInput.toLowerCase();
-
-  if (emailInput !== lowerCaseEmail) {
-    document.getElementById('error').innerHTML = 'Email must be in lowercase!';
-    return false;
-  }
-  document.getElementById('error').innerHTML = '';
-  return true;
-}
-
-const submitform = document.getElementById('submitform');
-submitform.addEventListener('click', () => {
-  validateEmail();
-});
